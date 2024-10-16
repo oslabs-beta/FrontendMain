e1871eac1a1c1687ae314190840de8ec1684f546
David <davidjs314@yahoo.com>
2024-10-16 15:56:18 -0400

    changed port in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:3008',
         changeOrigin: true,
         secure: false,
         ws: true
