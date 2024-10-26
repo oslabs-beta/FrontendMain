44ba492b096fff4442194c2ff10a31d604189770
Allie Itczak <allie.itczak@gmail.com>
2024-10-26 11:33:10 -0500

    updated form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,5 @@
 // import * as React from 'react';
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import OAuth from "./OAuth";
 import { motion } from "framer-motion";
 import "../css/App.css";
