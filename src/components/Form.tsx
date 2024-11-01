aede867096b1071d601d27ec7acbb2f0f63a451b
David <davidjs314@yahoo.com>
2024-10-31 20:27:32 -0400

    Fixes Oauth

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -218,7 +218,7 @@ function Form({ setQueries }: FormProps): JSX.Element {
     setSignInEmailIsNotValid(false);
     setIsSignInEmailNotValid(false);
     if (validateEmail(email)) {
-      fetch(`${backend_url}/api/signin`, {
+      fetch(`/api/signin`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email: email, password: password }),
